a7b72c10241c03ba36df0d120af7914a6002cbf9
김종민 <larva3203@gmail.com>
2026-08-30 21:37:59 +0900

    feat: integrate album methodology resolver

--- lib/alternative-evidence/index.ts
+++ lib/alternative-evidence/index.ts
@@ -30,3 +30,5 @@ export * from './identityFoundation';
 export * from './albumTemporalSnapshot';
 export * from './yes24RetailAdapter';
 export * from './directProviderEvidence';
+export * from './albumMethodology';
+export * from './albumSyntheticValidation';
