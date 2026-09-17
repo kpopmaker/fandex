c620197f263b47181abf7e2bd0230aa0802ed30c
kpopmaker <larva3203@gmail.com>
2026-09-17 19:55:04 +0900

    research: derive Hanteo first-week normalization readiness

--- lib/alternative-evidence/hanteoFirstWeekProductionEvidenceResearch.ts
+++ lib/alternative-evidence/hanteoFirstWeekProductionEvidenceResearch.ts
@@ -1,7 +1,9 @@
 import {
   HANTEO_ALBUM_PRODUCTION_EVIDENCE,
+  type AlbumNormalizationFreezeInputs,
   type AlbumProviderProductionEvidence,
 } from './albumProductionReadinessResearch';
+import { ALBUM_NORMALIZATION_INTERNAL_DEFINITION_READINESS } from './albumNormalizationResearch';
 import { HANTEO_FIRST_WEEK_PERIOD_RESEARCH_DESCRIPTOR } from './hanteoFirstWeekPeriodResearch';
 
 export const HANTEO_FIRST_WEEK_PRODUCTION_EVIDENCE_RESEARCH_VERSION =
@@ -19,3 +21,17 @@ export const HANTEO_FIRST_WEEK_PRODUCTION_EVIDENCE_RESEARCH = Object.freeze({
     ...HANTEO_FIRST_WEEK_PERIOD_RESEARCH_DESCRIPTOR.evidenceUrls,
   ]),
 }) satisfies AlbumProviderProductionEvidence;
+
+export const HANTEO_FIRST_WEEK_NORMALIZATION_FREEZE_INPUTS_RESEARCH = Object.freeze({
+  sourceAuthorizationResolved: false,
+  providerPeriodDefinitionResolved:
+    HANTEO_FIRST_WEEK_PRODUCTION_EVIDENCE_RESEARCH.periodSemantics === 'verified',
+  baselineDefinitionResolved:
+    ALBUM_NORMALIZATION_INTERNAL_DEFINITION_READINESS.baselineDefinitionResolved,
+  crossReleaseComparabilityResolved:
+    ALBUM_NORMALIZATION_INTERNAL_DEFINITION_READINESS.crossReleaseComparabilityResolved,
+  transformationRuleDefined:
+    ALBUM_NORMALIZATION_INTERNAL_DEFINITION_READINESS.transformationRuleDefined,
+  revisionPolicyResolved:
+    ALBUM_NORMALIZATION_INTERNAL_DEFINITION_READINESS.revisionPolicyResolved,
+}) satisfies AlbumNormalizationFreezeInputs;
