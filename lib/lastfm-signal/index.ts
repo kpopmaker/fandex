d823853b426e302b42598d8d986cd2da2932a72b
kpopmaker <larva3203@gmail.com>
2026-09-18 10:15:47 +0900

    fix(lastfm): normalize v137 export newline

--- lib/lastfm-signal/index.ts
+++ lib/lastfm-signal/index.ts
@@ -7,4 +7,4 @@ export * from './historicalWindow';
 export * from './shadowVariableAdapter';
 export * from './shadowScoringReconciliation';
 export * from './repeatedShadowReconciliation';
-export * from './historicalShadowCheckpoint';\n
+export * from './historicalShadowCheckpoint';
